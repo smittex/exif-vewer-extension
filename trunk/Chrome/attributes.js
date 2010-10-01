5cd39c9ed3f1ba00fab3c11bf3bfc0788dbe7f0a
andry.virvich <andry.virvich@8d88ea5a-761c-6085-c8a3-e5752ff37892>
2010-10-01 08:22:35 +0000

    add options page

---
+++ trunk/Chrome/attributes.js
@@ -0,0 +1,272 @@
+var exifAttributes = {
+	ExifVersion: {
+		visible: false
+	},
+	FlashpixVersion: {
+		visible: false
+	},
+	ColorSpace: {
+		visible: false
+	},
+	PixelXDimension: {
+		visible: false
+	},
+	PixelYDimension: {
+		visible: false
+	},
+	ComponentsConfiguration: {
+		visible: false
+	},
+	CompressedBitsPerPixel: {
+		visible: false
+	},
+	MakerNote: {
+		visible: false
+	},
+	UserComment: {
+		visible: false
+	},
+	RelatedSoundFile: {
+		visible: false
+	},
+	DateTimeOriginal: {
+		visible: true
+	},
+	DateTimeDigitized: {
+		visible: false
+	},
+	SubsecTime: {
+		visible: false
+	},
+	SubsecTimeOriginal: {
+		visible: false
+	},
+	SubsecTimeDigitized: {
+		visible: false
+	},
+	ExposureTime: {
+		visible: true
+	},
+	FNumber: {
+		visible: true
+	},
+	ExposureProgram: {
+		visible: true
+	},
+	SpectralSensitivity: {
+		visible: false
+	},
+	ISOSpeedRatings: {
+		visible: true
+	},
+	OECF: {
+		visible: false
+	},
+	ShutterSpeedValue: {
+		visible: false
+	},
+	ApertureValue: {
+		visible: false
+	},
+	BrightnessValue: {
+		visible: false
+	},
+	ExposureBias: {
+		visible: false
+	},
+	MaxApertureValue: {
+		visible: false
+	},
+	SubjectDistance: {
+		visible: false
+	},
+	MeteringMode: {
+		visible: true
+	},
+	LightSource: {
+		visible: false
+	},
+	Flash: {
+		visible: true
+	},
+	SubjectArea: {
+		visible: false
+	},
+	FocalLength: {
+		visible: true
+	},
+	FlashEnergy: {
+		visible: false
+	},
+	SpatialFrequencyResponse: {
+		visible: false
+	},
+	FocalPlaneXResolution: {
+		visible: false
+	},
+	FocalPlaneYResolution: {
+		visible: false
+	},
+	FocalPlaneResolutionUnit: {
+		visible: false
+	},
+	SubjectLocation: {
+		visible: false
+	},
+	ExposureIndex: {
+		visible: false
+	},
+	SensingMethod: {
+		visible: false
+	},
+	FileSource: {
+		visible: false
+	},
+	SceneType: {
+		visible: false
+	},
+	CFAPattern: {
+		visible: false
+	},
+	CustomRendered: {
+		visible: false
+	},
+	ExposureMode: {
+		visible: false
+	},
+	WhiteBalance: {
+		visible: true
+	},
+	DigitalZoomRation: {
+		visible: false
+	},
+	FocalLengthIn35mmFilm: {
+		visible: false
+	},
+	SceneCaptureType: {
+		visible: false
+	},
+	GainControl: {
+		visible: false
+	},
+	Contrast: {
+		visible: false
+	},
+	Saturation: {
+		visible: false
+	},
+	Sharpness: {
+		visible: false
+	},
+	DeviceSettingDescription: {
+		visible: false
+	},
+	SubjectDistanceRange: {
+		visible: false
+	},
+	InteroperabilityIFDPointer: {
+		visible: false
+	},
+	ImageUniqueID: {
+		visible: false
+	},
+	ImageWidth: {
+		visible: false
+	},
+	ImageHeight: {
+		visible: false
+	},
+	ExifIFDPointer: {
+		visible: false
+	},
+	GPSInfoIFDPointer: {
+		visible: false
+	},
+	InteroperabilityIFDPointer: {
+		visible: false
+	},
+	BitsPerSample: {
+		visible: false
+	},
+	Compression: {
+		visible: false
+	},
+	PhotometricInterpretation: {
+		visible: false
+	},
+	Orientation: {
+		visible: false
+	},
+	SamplesPerPixel: {
+		visible: false
+	},
+	PlanarConfiguration: {
+		visible: false
+	},
+	YCbCrSubSampling: {
+		visible: false
+	},
+	YCbCrPositioning: {
+		visible: false
+	},
+	XResolution: {
+		visible: false
+	},
+	YResolution: {
+		visible: false
+	},
+	ResolutionUnit: {
+		visible: false
+	},
+	StripOffsets: {
+		visible: false
+	},
+	RowsPerStrip: {
+		visible: false
+	},
+	StripByteCounts: {
+		visible: false
+	},
+	JPEGInterchangeFormat: {
+		visible: false
+	},
+	JPEGInterchangeFormatLength: {
+		visible: false
+	},
+	TransferFunction: {
+		visible: false
+	},
+	WhitePoint: {
+		visible: false
+	},
+	PrimaryChromaticities: {
+		visible: false
+	},
+	YCbCrCoefficients: {
+		visible: false
+	},
+	ReferenceBlackWhite: {
+		visible: false
+	},
+	DateTime: {
+		visible: false
+	},
+	ImageDescription: {
+		visible: false
+	},
+	Make: {
+		visible: true
+	},
+	Model: {
+		visible: true
+	},
+	Software: {
+		visible: false
+	},
+	Artist: {
+		visible: false
+	},
+	Copyright: {
+		visible: false
+	}
+}
