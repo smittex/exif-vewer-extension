cd6f923f70379d22800525b692db8ff8aa9e5838
andry.virvich <andry.virvich@8d88ea5a-761c-6085-c8a3-e5752ff37892>
2010-10-27 03:55:52 +0000

    add options button to exif dlg

--- Chrome/exif_inject.js
+++ Chrome/exif_inject.js
@@ -1,5 +1,12 @@
 function exif_inject(data, title, closeButton){
-		$("<div />").html(data).dialog({
+		$("<div />").html(data+"<hr />").append(
+			$("<a href='"+chrome.extension.getURL("options.html")+"' />")
+				.text("Options")
+				.css("font-size","0.8em")
+				.css("padding","2px 5px 2px 5px")
+				.css("float", "right")
+				.button()
+		).dialog({
 			"minWidth" : 400,
 			"position": "center",
 			"title": title,
