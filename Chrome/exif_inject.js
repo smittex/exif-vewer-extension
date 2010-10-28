8150d64e59734705828585bf1504f2e85ece27f5
andry.virvich <andry.virvich@8d88ea5a-761c-6085-c8a3-e5752ff37892>
2010-10-28 05:31:22 +0000

    localize dialog

--- Chrome/exif_inject.js
+++ Chrome/exif_inject.js
@@ -1,14 +1,15 @@
 function exif_inject(data, title, closeButton){
+	var buttons = {};
+	buttons[chrome.i18n.getMessage("dialogConfigure")] = function() { document.location = chrome.extension.getURL("options.html"); };
+	buttons[chrome.i18n.getMessage("dialogClose")] = function() { $(this).dialog("close"); }
+	
 		$("<div />").html(data).dialog({
 			"minWidth" : 400,
 			"position": "center",
 			"resizable": false,
-			"title": title,
+			"title": chrome.i18n.getMessage("dialogTitle"),
 			"closeOnEscape": true,
 			"closeText": 'hide',
-			buttons: { 
-				"Configure": function() { document.location = chrome.extension.getURL("options.html"); },
-				"Close": function() { $(this).dialog("close"); },
-			}
+			"buttons": buttons
 		});
 }
