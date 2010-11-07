8f690a3c985482f65caa052b1fc323c84cfbaa01
andry.virvich <andry.virvich@8d88ea5a-761c-6085-c8a3-e5752ff37892>
2010-11-07 05:52:32 +0000

    version 1.2.0 added geolocation

--- trunk/Chrome/exif_inject.js
+++ trunk/Chrome/exif_inject.js
@@ -1,9 +1,12 @@
-function exif_inject(data, title, closeButton){
+function exif_inject(data){
 	var buttons = {};
 	buttons[chrome.i18n.getMessage("dialogConfigure")] = function() { document.location = chrome.extension.getURL("options.html"); };
 	buttons[chrome.i18n.getMessage("dialogClose")] = function() { $(this).dialog("close"); }
-	
-		$("<div />").html(data).dialog({
+		$("<div />").append(
+			$("<div />").attr("id", "ExifViewer").append(
+				$("<div />").attr("id", "ExifVewerTabData").html(data)
+			)
+		).dialog({
 			"minWidth" : 400,
 			"position": "center",
 			"resizable": false,
@@ -13,3 +16,18 @@ function exif_inject(data, title, closeButton){
 			"buttons": buttons
 		});
 }
+
+
+function exif_injectMap(sData){
+	var data = JSON.parse(unescape(sData));
+	$("<iframe />")
+		.appendTo($("#ExifViewer").attr("id", ""))
+		.css("width", "100%")
+		.css("height", "200px")
+		.css("border", "1px solid #cccccc")
+		.attr("src","http://maps.google.com/maps?f=q&source=s_q&q="+data.lat+","+data.lng+"&output=embed&type=G_NORMAL_MAP");
+
+	
+}
+
+
---
+++ trunk/Chrome/geo.js
@@ -0,0 +1,26 @@
+var Geo = {};  // Geo namespace, representing static class
+Geo.parseDMS = function(dms, ref) {
+
+  // check for signed decimal degrees without NSEW, if so return it directly
+  if (typeof dms === 'number') return Number(dms);
+  
+  // and convert to decimal degrees...
+  switch (dms.length) {
+    case 3:  // interpret 3-part result as d/m/s
+      var deg = dms[0]/1 + dms[1]/60 + dms[2]/3600; 
+      break;
+    case 2:  // interpret 2-part result as d/m
+      var deg = dms[0]/1 + dms[1]/60; 
+      break;
+    case 1:  // just d (possibly decimal) or non-separated dddmmss
+      var deg = dms[0];
+      // check for fixed-width unseparated format eg 0033709W
+      if (/[NS]/i.test(ref)) deg = '0' + deg;  // - normalise N/S to 3-digit degrees
+      if (/[0-9]{7}/.test(deg)) deg = deg.slice(0,3)/1 + deg.slice(3,5)/60 + deg.slice(5)/3600; 
+      break;
+    default:
+      return NaN;
+  }
+  if (/^-|[WS]$/i.test(ref)) deg = -deg; // take '-', west and south as -ve
+  return Number(deg);
+}
